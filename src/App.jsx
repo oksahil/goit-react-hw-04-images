b1790317d21833962cdbf6ef95e6b8f44d4f1236
oksahil <oksana.khylyk@gmail.com>
2023-02-01 22:48:40 +0100

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import PostFind from 'modules/PostFind/PostFind';
+import MainMenu from 'modules/MainMenu/MainMenu';
+import mainMenu from "./data/mainMenu.json";
+
 import './index.css';
 
 function App() {
   return (
     <div className="App">
+      <MainMenu items={mainMenu} />
         <PostFind/>
     </div>
   );
