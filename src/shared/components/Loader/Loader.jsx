c0c22245e517cfd31bb4c6fc92f6f3ae7a04aa49
oksahil <oksana.khylyk@gmail.com>
2023-02-01 15:14:40 +0100

    loader

--- src/shared/components/Loader/Loader.jsx
+++ src/shared/components/Loader/Loader.jsx
@@ -3,17 +3,19 @@ import { ThreeDots } from 'react-loader-spinner'
 
 const Loader= ()=> {
     return(
-        <ThreeDots  
+       <center> <ThreeDots  
             height="80" 
             width="80" 
             radius="9"
             color="#3f51b5" 
             ariaLabel="three-dots-loading"
             wrapperStyle={{
+                justifyContent:"center"
             }}
             wrapperClassName=""
             visible={true}
-        />
+           
+        /></center>
     )
 }
 
