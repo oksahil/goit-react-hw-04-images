eb0b242f8d714b46542ca48b11316f05361e8349
oksahil <oksana.khylyk@gmail.com>
2023-02-04 01:41:56 +0100

    Update PostFind.jsx

--- src/modules/PostFind/PostFind.jsx
+++ src/modules/PostFind/PostFind.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 
 import Modal from "shared/components/Modal/Modal";
 import {searchPosts} from '../../shared/services/postsApi';
@@ -11,83 +11,70 @@ import Message from "shared/components/Message/Message";
 
 import css from "./postFind.module.css";
 
-class PostFind extends Component {
-    state = {
-        find: "",
-        items: [],
-        loading: false,
-        error: null,
-        page: 1,
-        showModal: false,
-        postDetailes: null,
-    }
+const PostFind = () => {
+    const [find, setFind] = useState("");
+    const [items, setItems] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [page, setPage] = useState(1);
+    const [showModal, setShowModal] = useState(false);
+    const [postDetailes, setPostDetailes] = useState(null);
 
-    
-    componentDidUpdate(prevProps, prevState) {
-        const { find, page } = this.state;
-        if (prevState.find !== find || prevState.page !== page) {
-            this.fetchPost();
+    useEffect(() => {
+
+        if (find) {
+        const fetchPost = async() => {
+            try {
+                setLoading(true);
+            const hits = await searchPosts(find, page);
+            setItems((prevItems) => ([...prevItems, ...hits]))
+                }
+            catch (error) {
+                setError(error.message);
+                }
+            finally {
+                setLoading(false);
+                }    
+            }  
+            fetchPost();
         }
-    }
+    
+},[find, page, setLoading, setItems, setError])
 
-    async fetchPost () {
-        try {
-        this.setState({loading: true})
-        const { find, page } = this.state;
-        const hits = await searchPosts(find, page);
-        this.setState(({items}) => ({items: [...items, ...hits]}))
-            }
-        catch (error) {
-            this.setState({ error: error.message })
-            }
-        finally {
-            this.setState({loading: false})
-            }    
+const onSearchPost = ({ prevFind, find}) => {
+    if (prevFind !== find) {
+        setFind(find);
+        setItems([]);
+        setPage(1);
+        }
+    }   
+    
+const showPost = ({id, largeImageURL }) => {
+    setPostDetailes({ id, largeImageURL });
+    setShowModal(true);
 }
 
-    searchPost = ({ find}) => {
-       if (find !== this.state.find) {
-            this.setState({ find, items: [], page: 1 });
-        }
-    }
-    
-    loadMore = () => {
-        this.setState(({page}) => ({page: page + 1}))
-    }
+const loadMore = () => {
+    setPage((prevPage) => (prevPage + 1));
+}
 
-    showPost = ({id, largeImageURL }) => {
-        this.setState({
-            postDetailes: {
-                id,
-                largeImageURL,
-            },
-            showModal: true,
-    })
-    }
+const closeModal = () => {
+    setShowModal(false);
+    setPostDetailes(null);
+}
     
-    closeModal = () => {
-        this.setState({
-            showModal: false,
-            postDetailes: null,
-        })
-    }
-
-    render() {
-        const { items, loading, error, showModal, postDetailes, find} = this.state;
-        const { searchPost, loadMore, showPost, closeModal } = this;
-        return (
-            <>
-                <PostFindForm onSubmit={searchPost} />
-                {(!find || items.length === 0) && <Message message="Please enter a valid keyword to search for photos." />}
-                {error && <Message message="Sorry... No results were found for this query! 
+    return (
+        <>
+            <PostFindForm onSubmit={onSearchPost} />
+            {(!find || items.length === 0) && <Message message="Please enter a valid keyword to search for photos." />}
+            {error && <Message message="Sorry... No results were found for this query! 
                                             Perhaps you are sending an empty request. Please enter a keyword to search." />}
-                {loading && <Loader className={css.loader} />}
-                <PostFindList showPost={showPost} items={items} />
-                {Boolean(items.length) && <Button onClick={loadMore}>Load more...</Button>}
-                {showModal && <Modal close={closeModal}><PostDetailes {...postDetailes} /></Modal>}
-            </>
-        )
-    }
+            {loading && <Loader className={css.loader} />}
+            <PostFindList showPost={showPost} items={items} />
+            {Boolean(items.length) && <Button onClick={loadMore}>Load more...</Button>}
+            {showModal && <Modal close={closeModal}><PostDetailes {...postDetailes} /></Modal>}
+        </>
+    )    
 }
-
+    
 export default PostFind;
