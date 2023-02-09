738a136cbea189fba8cd82ded1b0fab4d82c78cc
oksahil <oksana.khylyk@gmail.com>
2023-02-09 16:28:10 +0100

    Update PostFindForm.jsx

--- src/modules/PostFind/PostFindForm/PostFindForm.jsx
+++ src/modules/PostFind/PostFindForm/PostFindForm.jsx
@@ -1,44 +1,46 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 
-// import initialState from "./initialState";
+import initialState from "./initialState";
 
 import css from "./postFindForm.module.css"
 
-const PostFindForm = ({ onSubmit}) => {
-    const [find, setFind] = useState("");
+const PostFindForm = ({ onSubmit }) => {
+    const [state, setState] = useState({ ...initialState });
 
-    const handleChange = ({target}) => {
-        const {value} = target;
-        setFind(value);
-    };
+const handleChange = ({target}) => {
+    const { name, value } = target;
+    setState(prevState => {
+        return { ...prevState, [name]: value };
+    })
+}
 
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onSubmit(find);
-    };
-    
-    return (
+        const handleSubmit = (e) => {
+            e.preventDefault();
+            onSubmit(find);
+        };
+        const { find } = state;
+        return (
             <header className={css.findBar}>
                 <form className={css.form} onSubmit={handleSubmit}>
                     <button type="submit" className={css.findFormBtn}>
-                    <span className={css.labelText}>Search</span>
+                        <span className={css.labelText}>Search</span>
                     </button>
 
-                <input
-                    className={css.findFormInput}
-                    name="find"
-                    value={find}
-                    onChange={handleChange}
-                    type="text"
+                    <input
+                        className={css.findFormInput}
+                        name="find"
+                        value={find}
+                        onChange={handleChange}
+                        type="text"
                         placeholder="Search photos"
-                        required 
-                />
+                        required
+                    />
                 </form>
             </header>
-            )
-}
+        )
+    }
+
 
 export default PostFindForm;
 
